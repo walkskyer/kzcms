5358023f0d62947fee7049798feddf3f80158c2d
walkskyer <walkskyer@qq.com>
2013-01-24 22:01:45 +0800

    添加模型

--- public/application/models/uploads_model.php
+++ public/application/models/uploads_model.php
@@ -3,12 +3,12 @@
  * Created by JetBrains PhpStorm.
  * User: walkskyer
  * Date: 13-1-24
- * Time: 下午9:28
+ * Time: 下午9:53
  * To change this template use File | Settings | File Templates.
  */
-class Admin_model extends MY_Model{
+class Uploads_model extends MY_Model{
     public function __construct(){
         parent::__construct();
-        $this->_tableName='admin';
+        $this->_tableName='uploads';
     }
 }
