32f6253618b9272afc64df846fee783525878f08
walkskyer <walkskyer@qq.com>
2013-01-25 22:19:21 +0800

    将menulist()方法挪到admin控制器中。

--- public/application/controllers/manage/admin.php
+++ public/application/controllers/manage/admin.php
@@ -6,15 +6,60 @@
  * Time: 下午10:25
  * To change this template use File | Settings | File Templates.
  */
-class admin extends  MY_Controller
+class admin extends MY_Controller
 {
     private $_data = array();
-    public  function index(){
 
-        $this->_data['menuList']= $this->menuList();
-        $this->load->view("manage/head",$this->_data);
+    public function index()
+    {
+
+        $this->_data['menuList'] = $this->menuList();
+        $this->load->view("manage/head", $this->_data);
         $this->load->view("manage/body");
         $this->load->view("manage/foot");
     }
 
+    /*
+         * 返回后台menu相关数组
+         */
+    protected function menuList()
+    {
+        $data = array(
+            array(
+                'itemname' => '常用操作',
+                'class' => 'thisclass',
+                '_for' => 'common',
+                'href' => '',
+                'display' => "block",
+                'childs' => array(
+                    array('url' => '', 'name' => '网站设置'),
+                    array('url' => '', 'name' => '网站设置'),
+                )
+            ),
+            array(
+                'itemname' => '内容管理',
+                'class' => '',
+                '_for' => 'content',
+                'href' => '',
+                'display' => "none",
+                'childs' => array(
+                    array('url' => '', 'name' => '网站设置'),
+                    array('url' => '', 'name' => '网站设置'),
+                )
+            ),
+            array(
+                'itemname' => '产品管理',
+                'class' => '',
+                '_for' => 'product',
+                'href' => '',
+                'display' => "none",
+                'childs' => array(
+                    array('url' => '', 'name' => '网站设置'),
+                    array('url' => '', 'name' => '网站设置'),
+                )
+            )
+        );
+
+        return $data;
+    }
 }
